3e3a4daeeca8e4a08b2dabdc1efed11ed0536211
NorthWestEast <72236970+NorthWestEast@users.noreply.github.com>
2021-10-20 20:05:16 +0300

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 // Enables CORS on Express.js
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", process.env.ORIGIN); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
   next();
